b1e1552e60cbf5610a8622e63d7a14f91c01b5da
ttcool <ttcool@live.cn>
2016-12-22 18:41:03 +0800

    first commit

---
+++ spy.py
@@ -0,0 +1,19 @@
+
+import urllib2
+
+def download(url, user_agent='wswp', num_retries=2):
+    print 'Downloading:', url
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
+    try:
+        html = urllib2.urlopen(request).read()
+    except urllib2.URLError as e:
+        print 'Download error:', e.reason
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                # retry 5XX HTTP errors
+                return download(url, user_agent, num_retries-1)
+    return html
+
+print download('http://www.blingstorm.com.cn')
